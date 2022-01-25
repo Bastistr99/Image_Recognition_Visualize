fbe3dfb6397295fc090aaecb21edccc2ef4c0cc5
moritz <moritz-23@web.de>
2022-01-25 19:40:17 +0100

    Test vom Noob

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
+#test
 
 @app.route("/")
 def first_route():
